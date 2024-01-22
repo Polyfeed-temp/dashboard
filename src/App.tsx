74fe19c887ee9d8f7a1bfcf965e21b36ea575434
cooi0004 <cooi0004@student.monash.edu>
2024-01-23 00:28:19 +1100

    fixing styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,85 +1,80 @@
 import React, {useState, useEffect} from "react";
-
-import {HighlightFeed} from "./components/HighlightFeed";
 import {NavBar} from "./components/NavBar";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
-import {MultiSectionProgressBar} from "./components/ProgressBar";
-import {AssignmentView} from "./components/AssignmentView";
-import {UnitSummary} from "./components/UnitSummary";
-import BarChartComponent from "./components/dataVis/BarChart";
-import axios from "axios";
+
 import {OverviewPage} from "./pages/OverviewPage";
 import SignUpPage from "./pages/SignUpPage";
 import {LoginPage} from "./pages/LoginPage";
 import {UnitSummaryPage} from "./pages/UnitPage";
 import {Feedback} from "./types";
 import UserService from "./services/user.service";
-
+import {useUserState} from "./store/UserContext";
+import "react-toastify/dist/ReactToastify.css";
+import {ToastContainer} from "react-toastify";
+import {AllUnitsPage} from "./pages/AllUnitsPage";
 function App() {
   const [unitFeedBacks, setUnitFeedBacks] = useState([]);
   const [feedbacks, setFeedback] = useState<Feedback[]>([]);
+  const user = useUserState();
   const userService = new UserService();
-  useEffect(() => {
-    userService.getUserFeedbacks().then((res) => setFeedback(res));
-  }, []);
+  const [groupedByUnitCode, setGroupedByUnitCode] = useState<{
+    [key: string]: Feedback[];
+  } | null>(null);
 
-  const groupedByUnitCode = feedbacks.reduce((acc, item) => {
-    if (!acc[item.unitCode]) {
-      acc[item.unitCode] = [];
+  useEffect(() => {
+    if (!user.login) {
+      return;
     }
-    acc[item.unitCode].push(item);
-    return acc;
-  }, {} as {[key: string]: Feedback[]});
-
-  console.log(groupedByUnitCode);
+    userService.getUserFeedbacks().then((res) => setFeedback(res));
+  }, [user.login]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/annotation/all")
-      .then((response) => response.json())
-      .then((data) => {
-        setUnitFeedBacks(data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data: ", error);
-      });
-  }, []); // Empty dependency array ensures this runs once
+    if (feedbacks.length === 0) {
+      return;
+    }
+    const mapped = feedbacks.reduce((acc, item) => {
+      if (!acc[item.unitCode]) {
+        acc[item.unitCode] = [];
+      }
+      acc[item.unitCode].push(item);
+      return acc;
+    }, {} as {[key: string]: Feedback[]});
+    setGroupedByUnitCode(mapped);
+  }, [feedbacks]);
 
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  useEffect(() => {
-    fetch("http://localhost:8000/api/login/users/me", {
-      method: "GET",
-      credentials: "include", // necessary to include HttpOnly cookies
-    })
-      .then((response) => {
-        if (response.ok) {
-          setIsLoggedIn(true);
-        } else {
-          setIsLoggedIn(false);
-        }
-      })
-      .catch((error) => {
-        console.error("Error validating token:", error);
-        setIsLoggedIn(false);
-      });
-  }, []);
+  //iterate grouped by unit code then each highlight and only get the common theme
 
   return (
     <div>
+      (
       <BrowserRouter>
-        <NavBar unitCodes={Object.keys(groupedByUnitCode)}></NavBar>
+        <NavBar unitCodes={Object.keys(groupedByUnitCode || {})}></NavBar>
 
         <Routes>
-          <Route path="/" element={<OverviewPage />}></Route>
           <Route path="/signup" element={<SignUpPage />}></Route>
           <Route path="/login" element={<LoginPage />}></Route>
-
-          <Route
-            path="/feedback/:unitCode"
-            element={<UnitSummaryPage groupedByUnitCode={groupedByUnitCode} />}
-          ></Route>
+          {groupedByUnitCode && (
+            <>
+              <Route
+                path="/"
+                element={<OverviewPage groupedByUnitCode={groupedByUnitCode} />}
+              ></Route>
+              <Route
+                path="/feedback/:unitCode"
+                element={
+                  <UnitSummaryPage groupedByUnitCode={groupedByUnitCode} />
+                }
+              ></Route>
+              <Route
+                path="/all"
+                element={<AllUnitsPage feedbacks={feedbacks} />}
+              ></Route>
+            </>
+          )}
         </Routes>
       </BrowserRouter>
+      )
+      <ToastContainer position="bottom-right" />
     </div>
   );
 }
